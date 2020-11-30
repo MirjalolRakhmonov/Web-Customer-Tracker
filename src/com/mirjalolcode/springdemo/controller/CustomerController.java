cfb8c1c69a3ff659f93333302c7df1026cfc0627
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-30 19:17:43 +0100

    Update Customer - Prepopulating the Form

--- src/com/mirjalolcode/springdemo/controller/CustomerController.java
+++ src/com/mirjalolcode/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mirjalolcode.springdemo.entity.Customer;
 import com.mirjalolcode.springdemo.service.CustomerService;
@@ -16,15 +17,17 @@ import com.mirjalolcode.springdemo.service.CustomerService;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	// need to inject the customer service
+
+	// need to inject our customer service
 	@Autowired
 	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		// get customers from the dao
-		List<Customer> theCustomers=customerService.getCustomers();
 		
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
+				
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
 		
@@ -33,8 +36,9 @@ public class CustomerController {
 	
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model theModel) {
+		
 		// create model attribute to bind form data
-		Customer theCustomer=new Customer();
+		Customer theCustomer = new Customer();
 		
 		theModel.addAttribute("customer", theCustomer);
 		
@@ -45,8 +49,22 @@ public class CustomerController {
 	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
 		
 		// save the customer using our service
-		customerService.saveCustomer(theCustomer);
+		customerService.saveCustomer(theCustomer);	
 		
 		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+									Model theModel) {
+		
+		// get the customer from our service
+		Customer theCustomer = customerService.getCustomer(theId);	
+		
+		// set customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer", theCustomer);
+		
+		// send over to our form		
+		return "customer-form";
 	}
 }
--- src/com/mirjalolcode/springdemo/service/CustomerServiceImpl.java
+++ src/com/mirjalolcode/springdemo/service/CustomerServiceImpl.java
@@ -2,10 +2,9 @@ package com.mirjalolcode.springdemo.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mirjalolcode.springdemo.dao.CustomerDAO;
 import com.mirjalolcode.springdemo.entity.Customer;
@@ -14,19 +13,27 @@ import com.mirjalolcode.springdemo.entity.Customer;
 public class CustomerServiceImpl implements CustomerService {
 
 	// need to inject customer dao
-	@Autowired
-	private CustomerDAO customerDAO;
-	
-	@Override
-	@Transactional
-	public List<Customer> getCustomers() {
-		return customerDAO.getCustomers();
-	}
+		@Autowired
+		private CustomerDAO customerDAO;
+		
+		@Override
+		@Transactional
+		public List<Customer> getCustomers() {
+			return customerDAO.getCustomers();
+		}
 
-	@Override
-	@Transactional
-	public void saveCustomer(Customer theCustomer) {
-		customerDAO.saveCustomer(theCustomer);
-	}
+		@Override
+		@Transactional
+		public void saveCustomer(Customer theCustomer) {
+
+			customerDAO.saveCustomer(theCustomer);
+		}
+
+		@Override
+		@Transactional
+		public Customer getCustomer(int theId) {
+			
+			return customerDAO.getCustomer(theId);
+		}
 
 }
